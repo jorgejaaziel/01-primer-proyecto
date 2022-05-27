4d6340abbc67d7190959dcb7cb0a49108845f61e
Montemayor Leos <jmontemayor@sigma-alimentos.com>
2022-05-27 14:02:19 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+//import { ContadorComponent } from './contador/contador/contador.component';
+import { ContadorModule } from './contador/contador/contador.module';
+//import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { HeroesModule } from './heroes/heroes.module';
+//import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   // ContadorComponent
+    //HeroeComponent,
+    //ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
