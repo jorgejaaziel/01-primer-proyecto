189e457e65b6590aafa35810df0ad1b9823778a8
Montemayor Leos <jmontemayor@sigma-alimentos.com>
2022-05-27 21:57:57 -0500

    First version: Modulo DBZ, Designe, FormsModule, ngModel, Show list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 //import { ContadorComponent } from './contador/contador/contador.component';
 import { ContadorModule } from './contador/contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 //import { HeroeComponent } from './heroes/heroe/heroe.component';
 import { HeroesModule } from './heroes/heroes.module';
 //import { ListadoComponent } from './heroes/listado/listado.component';
@@ -17,7 +18,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
